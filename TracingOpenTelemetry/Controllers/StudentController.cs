232154929233295b1be5f13e528d13dcefcfa312
qasas145 <mohamed.sayed14527@yahoo.com>
2025-06-07 19:19:58 +0300

    Initial Commit

---
+++ TracingOpenTelemetry/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TracingOpenTelemetry.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger<StudentController> _logger;
+        public StudentController(ILogger<StudentController> logger) => _logger = logger;
+
+        public const string TelemetrySource = "MuhApplication";
+        [HttpGet("getdata")]
+        public async Task<IActionResult> GetData()
+        {
+            _logger.LogTrace("Starting the activity");
+
+            ActivitySource _source = new ActivitySource(TelemetrySource);
+
+            using (var activity = _source.StartActivity("FirstCallActivity"))
+            {
+                DateTime d1 = DateTime.Now;
+                activity?.SetTag("TimeOfCall", d1.ToString());
+                Thread.Sleep(4000);
+                DateTime d2 = DateTime.Now;
+                activity?.SetTag("TimeAfterCall", d2.ToString());
+                activity?.SetTag("Duration", d2 - d1);
+            }
+
+            // 
+            using (var activity = _source.StartActivity("SecondCallActivity"))
+            {
+                DateTime d1 = DateTime.Now;
+                activity?.SetTag("TimeOfCall", d1.ToString());
+                Thread.Sleep(4000);
+                DateTime d2 = DateTime.Now;
+                activity?.SetTag("TimeAfterCall", d2.ToString());
+                activity?.SetTag("Duration", d2 - d1);
+            }
+
+            return Ok(new { weather = "Sunny", data = "Some sort of data" });
+        }
+    }
+}
---
+++ TracingOpenTelemetry/Program.cs
@@ -0,0 +1,48 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddHttpClient();
+
+
+var serviceName = "MuhApplication";
+
+
+builder.Services.AddOpenTelemetry().WithTracing(tracerProviderBuilder =>
+{
+    tracerProviderBuilder
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName).AddTelemetrySdk())
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddSource(serviceName)
+            .AddConsoleExporter()
+            .AddJaegerExporter();
+}).WithMetrics(options =>
+{
+    options.AddHttpClientInstrumentation()
+    .AddConsoleExporter()
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName).AddTelemetrySdk()).AddMeter("Meter1");
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
